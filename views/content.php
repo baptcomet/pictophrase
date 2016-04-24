10502a8c45d770276826473c9bda32ec92fc1572
baptcomet <baptiste.comet@gmail.com>
2016-04-24 21:17:18 +0200

    Avancement général

--- views/content.php
+++ views/content.php
@@ -28,5 +28,17 @@ $currentArbre = $arbre;
         </div>
     </div>
 </div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4>Phrase</h4>
+            </div>
+            <div class="panel-body" id="panel-phrase-body">
+                <br/>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php include 'partial/script.php' ?>
--- views/partial/script.php
+++ views/partial/script.php
@@ -10,20 +10,30 @@
             var pictoId = $(this).attr('id');
             var picto = pictogrammes[pictoId];
             var nouveauxPictos = arbre[pictoId];
-            console.log(nouveauxPictos);
-
-            // TODO gestion du cas ou ya plus de nouveaux pictos
 
             var liste = $('#liste');
             liste.fadeOut('slow', function() {
                 var listeHtml = '';
-                for (var nouveauPictoId in nouveauxPictos) {
-                    var nouveauPicto = pictogrammes[nouveauPictoId];
-                    listeHtml += '<div class="col-md-2">';
-                    listeHtml += '<div class="panel panel-default panel-clickable" id="' + nouveauPictoId + '">';
-                    listeHtml += '<img src="assets/pictogrammes/' + nouveauPicto['image'] + '" class="img-responsive">';
-                    listeHtml += '<div class="panel-body">' + nouveauPicto['texte'] + '</div>';
-                    listeHtml += '</div>';
+                if (nouveauxPictos) {
+                    for (var nouveauPictoId in nouveauxPictos) {
+                        var nouveauPicto = pictogrammes[nouveauPictoId];
+                        listeHtml += '<div class="col-md-2">';
+                        listeHtml += '<div class="panel panel-default panel-clickable" id="' + nouveauPictoId + '">';
+                        listeHtml += '<img src="assets/pictogrammes/' + nouveauPicto['image'] + '" class="img-responsive">';
+                        listeHtml += '<div class="panel-body">';
+                        listeHtml += nouveauPicto['texte'];
+                        listeHtml += '<br/>';
+                        listeHtml += '<audio controls>';
+                        listeHtml += '<source src="assets/sons/' + nouveauPicto['son'] + '" type="audio/mpeg">';
+                        listeHtml += 'Your browser does not support the audio element.';
+                        listeHtml += '</audio>';
+                        listeHtml += '</div>';
+                        listeHtml += '</div>';
+                        listeHtml += '</div>';
+                    }
+                } else {
+                    listeHtml = '<div class="col-md-12">';
+                    listeHtml += '<div class="alert alert-warning">Il n\'y a plus de pictogramme</div>';
                     listeHtml += '</div>';
                 }
                 liste.html(listeHtml);
@@ -33,6 +43,21 @@
 
             var breadcrumbHtml = '<li>' + picto['texte'] + '</li>';
             $('#phrase').append(breadcrumbHtml);
+
+            var phrasePanelPart = '<div class="col-md-2">';
+            phrasePanelPart += '<div class="panel panel-default">';
+            phrasePanelPart += '<img src="assets/pictogrammes/' + picto['image'] + '" class="img-responsive">';
+            phrasePanelPart += '<div class="panel-body">';
+            phrasePanelPart += picto['texte'];
+            phrasePanelPart += '<br/>';
+            phrasePanelPart += '<audio controls>';
+            phrasePanelPart += '<source src="assets/sons/' + picto['son'] + '" type="audio/mpeg">';
+            phrasePanelPart += 'Your browser does not support the audio element.';
+            phrasePanelPart += '</audio>';
+            phrasePanelPart += '</div>';
+            phrasePanelPart += '</div>';
+            phrasePanelPart += '</div>';
+            $('#panel-phrase-body').append(phrasePanelPart);
         });
     }
 
